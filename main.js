625dd68a9c5dfb6024049cc9c8390db2783b5d4a
olygood <olivier.huttmacher@gmail.com>
2021-06-13 14:52:11 +0200

    mesh, color, and rotation torus

--- main.js
+++ main.js
@@ -13,9 +13,13 @@ import { WebGL1Renderer } from 'three';
 // const fullHeight = h *2;
 /**========================================================================
  *             windows.innerWidth = retourne la largeur de la fenetre
+ *              perspectiveCamera  = 1arg=degré, 2arge= ratio, 3arg=top near right 
  *========================================================================**/
+
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+
 scene.add( camera );
 const rederer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
@@ -23,21 +27,44 @@ const rederer = new THREE.WebGLRenderer({
 
 rederer.setPixelRatio(window.devicePixelRatio);
 rederer.setSize(window.innerWidth,window.innerHeight);
-camera.position.setZ(50);
+camera.position.setZ(100);
+
 rederer.render(scene, camera);
-const geometry = new THREE.TorusGeometry(10,3,16,100);
-const material = new THREE.MeshBasicMaterial({color:0xff6347, wireframe:true});
+
 /**========================================================================
  *                           3.Mesh = geometry + material
  *========================================================================**/
+const geometry = new THREE.TorusGeometry(25,4,10,100);
+const material = new THREE.MeshStandardMaterial({color:0xff6347});
 const torus = new THREE.Mesh(geometry,material);
 scene.add(torus);
-
+const pointLight = new THREE.PointLight(0xffffff); 
+pointLight.position.set(5,5,5);
+const ambientLight = new THREE.AmbientLight(120,82,255);
+scene.add(pointLight,ambientLight);
 /**========================================================================
  *                           Function
  *========================================================================**/
 function animate(){
     requestAnimationFrame(animate);
+    torus.rotation.x += 0.009;
+    torus.rotation.y += 0.009;
     rederer.render(scene,camera);
 }
 animate();
+
+// const loader = new THREE.FontLoader();
+// loader.load('fonts/helvetiker_regular.typeface.json', function(font){
+//     const geometry = new THREE.TextGeometry('hello ohstyle',{
+        
+//         font:font,
+//         size:80,
+//         height: 5,
+//         curveSegments: 12,
+//         bevelEnabled: true,
+//         bevelThickness: 10,
+//         bevelSize: 8,
+//         bevelOffset: 0,
+//         bevelSegments: 5
+//     });
+// });
